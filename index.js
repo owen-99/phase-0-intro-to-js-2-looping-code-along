cffdf501f7ebfe5736d0f0c254558028f18297db
OwenOchieng <owen.ochieng@student.moringaschool.com>
2022-06-10 11:23:22 +0300

    commit tree 32732b041c431391af93c52eb44017c32c790ef9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+  
+const names = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(names, surprise) {
+    const message =[]
+    for (let i = 0; i < gift.length; i++) {
+        
+        message.push(`wrapped ${gifts[i]} and added a bow!`)
+    }
+    return message;
+}
+
+let counter = -1
+function countDown(counter){
+  while (counter >= 0){
+    console.log(counter--)
+  }
+  
+}
